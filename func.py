b0a7a96023c73873e1c7d37c8a037be1a0286fe9
MadhavVats <43749601+MadhavVats@users.noreply.github.com>
2019-03-14 00:40:07 +0530

    Decorator for functions that should be traced

---
+++ func.py
@@ -0,0 +1,21 @@
+import sys
+from functools import wraps
+
+class TraceCalls(object):
+    """ Use as a decorator on functions that should be traced. Several
+        functions can be decorated - they will all be indented according
+        to their call depth.
+    """
+        
+    def __call__(self, fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            global functions_called
+            functions_called.append(fn.__name__)
+        return wrapper
+
+
+functions_called = []
+def fun_called():
+    return(functions_called)
+
